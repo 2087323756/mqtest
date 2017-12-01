4f66baf1826855fef7b4336cb5c21331d2c330d4
2087323756 <2087323756@qq.com>
2017-12-01 16:16:51 +0800

    添加项目文件。

---
+++ mqtest/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+
+namespace mqtest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static IConnectionFactory _factory = null;
+        static IConnection _connection = null;
+        static ITextMessage _message = null;
+        static ITextMessage _message2 = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_factory == null)
+                _factory = new ConnectionFactory(tbUrl.Text);
+            try
+            {
+                using (_connection = _factory.CreateConnection())
+                {
+                    using (ISession session = _connection.CreateSession(AcknowledgementMode.AutoAcknowledge))
+                    {
+                        IDestination destination = new Apache.NMS.ActiveMQ.Commands.ActiveMQTopic(tbTopic.Text);
+                        IMessageProducer producer = session.CreateProducer(destination);
+                        _message = producer.CreateTextMessage(richTextBox1.Text);
+                        producer.Send(_message, MsgDeliveryMode.Persistent, MsgPriority.Normal, TimeSpan.MaxValue);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
